df636ff8570252b92df0e9cd44f6d9d669f1994c
Megafry <Megafry@users.noreply.github.com>
2022-08-23 15:25:45 +0200

    Update countdown.js
    Replaced requestAnimationFrame with setInterval
    Added clearInterval if the countdown expired
    Added option to set custom text after expiration

--- countdown.js
+++ countdown.js
@@ -1,12 +1,13 @@
-const $ = elem => document.querySelector(elem);
+const cdwn = elem => document.querySelector(elem);
 
 const countdown = function(_config) {
-  const tarDate = $(_config.target).getAttribute('data-date').split('-');
+  const tarDate = cdwn(_config.target).getAttribute('data-date').split('-');
   const day = parseInt(tarDate[0]);
   const month = parseInt(tarDate[1]);
   const year = parseInt(tarDate[2]);
-  let tarTime = $(_config.target).getAttribute('data-time');
+  let tarTime = cdwn(_config.target).getAttribute('data-time');
   let tarhour, tarmin;
+  let animationFrame;
 
   if (tarTime != null) {
     tarTime = tarTime.split(':');
@@ -28,10 +29,10 @@ const countdown = function(_config) {
   // Set the date we're counting down to
   const countDownDate = new Date(year, month-1, day, tarhour, tarmin, 0, 0).getTime();
 
-  $(_config.target+' .day .word').innerHTML = _config.dayWord;
-  $(_config.target+' .hour .word').innerHTML = _config.hourWord;
-  $(_config.target+' .min .word').innerHTML = _config.minWord;
-  $(_config.target+' .sec .word').innerHTML = _config.secWord; 
+  cdwn(_config.target+' .day .word').innerHTML = _config.dayWord;
+  cdwn(_config.target+' .hour .word').innerHTML = _config.hourWord;
+  cdwn(_config.target+' .min .word').innerHTML = _config.minWord;
+  cdwn(_config.target+' .sec .word').innerHTML = _config.secWord;
 
   const updateTime = () => {
     // Get todays date and time
@@ -40,26 +41,31 @@ const countdown = function(_config) {
     // Find the distance between now an the count down date
     const distance = countDownDate - now;
 
+    if (distance < 0) {
+      cdwn(".countdown").innerHTML = `<span class="expired countdown__expired">${_config.expired ? _config.expired : 'expired'}</span>`;
+      clearInterval(animationFrame)
+      return false;
+    }
+
     // Time calculations for days, hours, minutes and seconds
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    requestAnimationFrame(updateTime);
+    //requestAnimationFrame(updateTime);
 
-    $(_config.target+' .day .num').innerHTML = addZero(days);
-    $(_config.target+' .hour .num').innerHTML = addZero(hours);
-    $(_config.target+' .min .num').innerHTML = addZero(minutes);
-    $(_config.target+' .sec .num').innerHTML = addZero(seconds);
+    cdwn(_config.target+' .day .num').innerHTML = addZero(days);
+    cdwn(_config.target+' .hour .num').innerHTML = addZero(hours);
+    cdwn(_config.target+' .min .num').innerHTML = addZero(minutes);
+    cdwn(_config.target+' .sec .num').innerHTML = addZero(seconds);
 
     // If the count down is over, write some text
-    if (distance < 0) {
-      $(".countdown").innerHTML = "EXPIRED";
-    }
-  }
 
-  updateTime();
+  }
+  //start
+  updateTime()
+  animationFrame = setInterval( updateTime, 1000)
 }
 
 const addZero = (x) => (x < 10 && x >= 0) ? "0"+x : x;
